716cdb7f1f6f10507c13ea9d706e7ba763ce23ee
tonyler <115975987+tonyler@users.noreply.github.com>
2023-10-20 21:42:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,21 +21,18 @@ async def send_update(price,server_id):
 @client.event
 async def on_ready():
         price = 0
+        print (client.guilds)
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Minswap DEX"))
-        while True:
-            if guild in client.guilds:
-                    price = get_price (price) #if there's an error, the bot will show 0 or the price of 2 minutes ago.
-                    price = round(price,4)
-                    print (f"New price: {price}₳")
-                    
-                    for guild in client.guilds:
-                        print (f"Updating data for {guild.name}")
-                        await send_update (price, guild.id)
-            else:
-                print ("The bot isn't a member of any server yet!")
+        price = get_price (price) #if there's an error, the bot will show 0 or the price of 2 minutes ago.
+        price = round(price,4)
+        print (f"New price: {price}₳")
+            
+        for guild in client.guilds:
+            print (f"Updating data for {guild.name}")
+            await send_update (price, guild.id)
                        
-            print ("***************************")
-            await asyncio.sleep (120)
+        print ("***************************")
+        await asyncio.sleep (120)
 
 
 bot_key = os.environ.get("DISCORD_KEY")
